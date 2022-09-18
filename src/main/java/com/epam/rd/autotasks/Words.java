880fbe0b1c22e6acf691b0d9f7d708b0584c1c27
yorqinjon <yorqinjonmahmudov93@gmail.com>
2022-09-18 18:03:13 +0500

    finally done

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -1,11 +1,90 @@
 package com.epam.rd.autotasks;
 
-
-import java.util.List;
+import java.util.*;
 
 public class Words {
 
     public String countWords(List<String> lines) {
-        throw new UnsupportedOperationException();
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        StringBuilder temp = new StringBuilder();
+
+        for (String s : lines) {
+            temp = new StringBuilder();
+
+            for (int i = 0; i < s.length(); i++) {
+                char ch = Character.toLowerCase(s.charAt(i));
+                if (ch == ' '
+                        || ch == '-'
+                        || ch == '/'
+                        || ch == '*'
+                        || ch == '.'
+                        || ch == ','
+                        || ch == '?'
+                        || ch == '!'
+                        || ch == ':'
+                        || ch == ';'
+                        || ch == '\''
+                        || ch == '“'
+                        || ch == '”'
+                        || ch == '—'
+                        || ch == '’'
+                        || ch == '('
+                        || ch == ')'
+                        || ch == '‘'
+                        || ch == '\"') {
+                    if (temp.length() >= 4) {
+
+                        if (hashMap.containsKey(temp.toString())) {
+                            hashMap.put(temp.toString(), hashMap.get(temp.toString()) + 1);
+                        } else {
+                            hashMap.put(temp.toString(), 1);
+                        }
+                    }
+                    temp = new StringBuilder();
+                    continue;
+                }
+                temp.append(ch);
+            }
+            if (temp.length() >= 4) {
+
+                if (hashMap.containsKey(temp.toString())) {
+                    hashMap.put(temp.toString(), hashMap.get(temp.toString()) + 1);
+                } else {
+                    hashMap.put(temp.toString(), 1);
+                }
+            }
+
+        }
+
+
+        LinkedList<Map.Entry<String, Integer>> entries = new LinkedList<>(hashMap.entrySet());
+        entries.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if (o1.getValue().equals(o2.getValue())) {
+                    return o1.getKey().compareTo(o2.getKey());
+                }
+                return o2.getValue() - o1.getValue();
+            }
+        });
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        boolean count = false;
+        for (Map.Entry<String, Integer> set : entries) {
+            if (set.getValue() >= 10 && count)
+                stringBuilder.append("\n");
+
+            if (set.getValue() >= 10) {
+                stringBuilder.append(set.getKey())
+                        .append(" - ")
+                        .append(set.getValue());
+            }
+            count = true;
+
+        }
+        return stringBuilder.toString();
     }
+
 }
